e517a682171b687096c7224294f870324b0dccf4
xxidk82xx <IiXxcjackxXiI@outlook.com>
2023-10-20 13:11:20 -0600

    set up repo for tank drive

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 /*----------------------------------------------------------------------------*/
 
 #include "vex.h"
+#include "robot-config.h"
 
 using namespace vex;
 
@@ -30,6 +31,10 @@ void pre_auton(void) {
 
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
+  BL.spin(forward);
+  FL.spin(forward);
+  FR.spin(forward);
+  BR.spin(forward);
 }
 
 /*---------------------------------------------------------------------------*/
@@ -61,14 +66,10 @@ void autonomous(void) {
 void usercontrol(void) {
   // User control code here, inside the loop
   while (1) {
-    // This is the main execution loop for the user control program.
-    // Each time through the loop your program should update motor + servo
-    // values based on feedback from the joysticks.
-
-    // ........................................................................
-    // Insert user code here. This is where you use the joystick values to
-    // update your motors, etc.
-    // ........................................................................
+    BL.setVelocity(Con1.Axis3.position(percent) + Con1.Axis4.position(percent), percent);
+    FL.setVelocity(Con1.Axis3.position(percent) + Con1.Axis4.position(percent), percent);
+    FR.setVelocity(Con1.Axis3.position(percent) - Con1.Axis4.position(percent), percent);
+    BR.setVelocity(Con1.Axis3.position(percent) - Con1.Axis4.position(percent), percent);
 
     wait(20, msec); // Sleep the task for a short amount of time to
                     // prevent wasted resources.
